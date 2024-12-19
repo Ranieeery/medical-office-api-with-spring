809d35a174ec424b872c2ca059cecaa608d1fc86
Ranieeery <raniery2003@hotmail.com>
2024-12-18 23:45:38 -0300

    feat: Add OpenAPI dependency and update security configurations to permit access to Swagger UI and API docs

--- src/main/java/dev/raniery/med/voll/api/infra/Security/SecurityConfigurations.java
+++ src/main/java/dev/raniery/med/voll/api/infra/Security/SecurityConfigurations.java
@@ -25,7 +25,14 @@ public class SecurityConfigurations {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.csrf(AbstractHttpConfigurer::disable).sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).authorizeHttpRequests(req -> req.requestMatchers("/login").permitAll().anyRequest().authenticated()).addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class).build();
+        return http.csrf(AbstractHttpConfigurer::disable).
+            sessionManagement(sm ->
+                sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(req ->
+                req.requestMatchers("/login", "/hello").permitAll()
+                .requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll()
+                .anyRequest().authenticated())
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class).build();
     }
 
     @Bean
