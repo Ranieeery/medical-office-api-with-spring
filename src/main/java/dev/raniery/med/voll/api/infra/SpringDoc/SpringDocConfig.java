34fa0312978f4443600edb1319d45e667dc922af
Ranieeery <raniery2003@hotmail.com>
2024-12-19 20:01:56 -0300

    feat: Enhance OpenAPI configuration with API metadata and contact information

--- src/main/java/dev/raniery/med/voll/api/infra/SpringDoc/SpringDocConfig.java
+++ src/main/java/dev/raniery/med/voll/api/infra/SpringDoc/SpringDocConfig.java
@@ -2,6 +2,9 @@ package dev.raniery.med.voll.api.infra.SpringDoc;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +16,18 @@ public class SpringDocConfig {
         return new OpenAPI()
             .components(new Components()
                 .addSecuritySchemes("bearer-key",
-                    new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
+                    new SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")))
+            .info(new Info()
+                .title("Med API")
+                .description("API Rest da aplicação simulando um consultório médico, contendo as funcionalidades de CRUD de médicos e de pacientes, além de agendamento e cancelamento de consultas")
+                .contact(new Contact()
+                    .name("Time Backend")
+                    .email("backend@med.com"))
+                .license(new License()
+                    .name("Apache 2.0")
+                    .url("https://www.apache.org/licenses/LICENSE-2.0.txt")));
     }
 }
